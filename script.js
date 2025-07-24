83ba5626cba38298927732bced35b69f5e698fbb
Emeç Yıldız <emecyildiz@posta.mu.edu.tr>
2025-07-24 10:06:08 +0300

    localstorage ayarlanması tamamlandı

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ const info = document.getElementById("input");
 
 //Sayfa Yüklenince çalışacak kod
 window.addEventListener("load",function(){
-    for(let i = 0; i < this.localStorage.length; i++){
-        const key = this.localStorage.key(i);
-        const value = this.localStorage.getItem(key);
-        todoOlustur(value);
-    }
+    loadTodosFromStorages();
+    
 });
 
 //Enter a basınca çalışacak kod 
-info.addEventListener("keydown", function(Event){
-    if(Event.key === "Enter"){
-        if(info.value === ""){
+info.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        if(info.value.trim() === ""){
             return;
-        }else{
+        } else {
             button.click();
         }
     }
@@ -27,11 +24,27 @@ button.addEventListener("click", function(){
     const text = info.value.trim();
     if (!text) return;
 
+     if(localStorage.getItem(text)) {
+        alert("Bu görev zaten mevcut!");
+        return;
+    }
+
     localStorage.setItem(text, text);
     todoOlustur(text);
     info.value = "";
+
+
 })
 
+function loadTodosFromStorages(){
+    for(let i = 0; i < localStorage.length; i++){
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        if(value) {
+            todoOlustur(value);
+        }
+    }
+}
 
 // Çalışması gereken ana fonksyon
 function todoOlustur(text) {
@@ -45,32 +58,29 @@ function todoOlustur(text) {
     newbutton.classList.add("button");
     newbutton1.classList.add("button");
     newlabel.classList.add("label-css");
+    newdiv.classList.add("div-css");
+    newlabel.textContent = text;
     
     //görevleri silen fonksyon
     newbutton.addEventListener("click", function(){
         newdiv.remove();
-        localStorage.removeItem(text)
+        localStorage.removeItem(text);
     })
     
     //yapılan görevleri ayarlayan fonkson
     newbutton1.addEventListener("click", function(){
         if(newlabel.classList.contains("label-css")){
             newlabel.classList.add("line");
-            newlabel.classList.remove("label-css")
+            newlabel.classList.remove("label-css");
         }else if(newlabel.classList.contains("line")){
-            newlabel.classList.add("label-css")
-            newlabel.classList.remove("line")
+            newlabel.classList.add("label-css");
+            newlabel.classList.remove("line");
         }
         
-    })
-    newdiv.classList.add("div-css")
-    newlabel.textContent = value;
+    });
     
     newdiv.appendChild(newlabel);
     newdiv.appendChild(newbutton);
     newdiv.appendChild(newbutton1);
     container.appendChild(newdiv); 
 };
-
-
-
